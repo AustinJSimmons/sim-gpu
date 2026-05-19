2669dace4d61a0cef3cb7d01e50f470cf8754549
austin.simmons <austin.simmons@ucalgary.ca>
2026-05-19 17:40:44 -0600

    Added what I think is working logic for ALU - needs testing

---
+++ include/ALU.h
@@ -0,0 +1,50 @@
+/*
+ * Header files will be used to define the following for each component:
+ * Structural Module:
+ * 1. External Ports (Pins - how it connects to the rest of the GPU)
+ * 2. Internal Signals (Wiring of component internals)
+ * 3. Child Modules (The internal components)
+ * 4. Constructor/Destructor
+ *
+ * Child Module:
+ * 1. Ports (Pins)
+ * 2. Internal State (Registers)
+ * 3. Process prototypes (Logic)
+ * 4. Constructor/Destructor
+ * **/
+#ifndef ALU_H
+#define ALU_H
+
+#include "systemc"
+using namespace sc_core;
+using namespace sc_dt;
+
+SC_MODULE(ALU) {
+  // 1. Ports (how it connects to the rest of the GPU)
+  // For input: At the most basic level I see the ALU needing
+  // in_a, in_b for our two input operands and some in_opp for
+  // our oppcode being executed as well as a clk input.
+  sc_in<bool> clk;
+  sc_in<sc_int<4>> opcode;
+  sc_in<sc_int<32>> input_a;
+  sc_in<sc_int<32>> input_b;
+
+  // For output: I see us needing a result output and maybe a
+  // few flag outputs to handle things like zero flags, overflow
+  // negative signs etc..
+  sc_out<sc_int<32>> result;
+
+  // 2. Process Logic
+  void execute();
+
+  // Constructor prototype
+  SC_CTOR(ALU) {
+    SC_METHOD(execute);
+    // sensitive << clk.pos();
+  }
+
+  // Destructor prototype
+  ~ALU();
+};
+
+#endif
---
+++ src/ALU.cpp
@@ -0,0 +1,31 @@
+/*
+ * cpp files will be used to fill out logic of functions
+ * for each module etc...
+ **/
+
+// Must include the component header
+#include "ALU.h"
+
+void ALU::execute() {
+  sc_int<4> op = opcode.read();
+  sc_int<32> a = input_a.read();
+  sc_int<32> b = input_b.read();
+
+  sc_int<32> res = 0;
+  // Define different actions for each Oppcode
+  switch (op) {
+  case 0x03:
+    res = a + b;
+    break; // ADD
+  case 0x04:
+    res = a - b;
+    break; // SUB
+  case 0x05:
+    res = a * b;
+    break; // MUL
+  default:
+    res = 0;
+  }
+
+  result.write(res);
+}
