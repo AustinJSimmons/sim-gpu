887e0a8a838e717880ea6841c54d2f5190f49f21
austin.simmons <austinsimmons098@gmail.com>
2026-06-25 11:10:07 -0600

    Boilerplate RF

--- include/RF.h
+++ include/RF.h
@@ -4,3 +4,19 @@
  * Defines a systemc module meant to represent our
  * SMs register file.
  **/
+
+#ifndef RF_H
+#define RF_H
+
+#include "systemc.h"
+using namespace sc_core;
+using namespace sc_dt;
+
+SC_MODULE(RF) {
+
+  SC_CTOR(RF) {}
+
+  ~RF();
+};
+
+#endif // !RF_H
